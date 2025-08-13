2b1f5a90dc4c9a408cc4eec502063e190e06c8dc
user17 <user17@psdc-eda04.psdc.org.my>
2025-08-13 11:23:54 +0800

    update flow to test golden or test own

--- work/w4.1/SV/SPI-Master-Slave/build.f
+++ work/w4.1/SV/SPI-Master-Slave/build.f
@@ -1,9 +1,3 @@
-//./spi_design/sclk_generator.v
-////./spi_design/spi_master.v
-//./spi_design/spi_master_own.sv
-//./spi_design/spi_slave.v
-//./spi_design/spi_top.v
-
 ./spi_design/bug_fix/spi_slave.v
 ./spi_design/bug_fix/spi_top.v
 ./spi_design/bug_fix/sclk_generator.v
